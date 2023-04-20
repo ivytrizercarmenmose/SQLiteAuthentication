d8ada2cfcc2108ca66f85d8b857a2d26fae64d11
Davinchi-x7 <dufgvjdu@gmail.com>
2023-04-20 10:39:57 +0300

    button added on dashboard

--- app/src/main/java/com/example/authenticationsqlite_carmen/Dashboard.kt
+++ app/src/main/java/com/example/authenticationsqlite_carmen/Dashboard.kt
@@ -1,11 +1,22 @@
 package com.example.authenticationsqlite_carmen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class Dashboard : AppCompatActivity() {
+class Dashboard : AppCompatActivity(){
+    lateinit var btnout:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        btnout = findViewById(R.id.btnout)
+
+        btnout.setOnClickListener {
+            val intent = Intent(this,loginActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
