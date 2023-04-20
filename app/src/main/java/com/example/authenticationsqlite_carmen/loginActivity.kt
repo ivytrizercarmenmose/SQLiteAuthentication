d17c253d1f7ed3ae13cac82877af9e88a8f02e61
Davinchi-x7 <dufgvjdu@gmail.com>
2023-04-20 10:23:39 +0300

    login

--- app/src/main/java/com/example/authenticationsqlite_carmen/loginActivity.kt
+++ app/src/main/java/com/example/authenticationsqlite_carmen/loginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.authenticationsqlite_carmen
 
+import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class loginActivity : AppCompatActivity() {
 
@@ -13,6 +16,7 @@ class loginActivity : AppCompatActivity() {
     lateinit var Password: EditText
     lateinit var Login :Button
     lateinit var register :Button
+    lateinit var db:SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +27,48 @@ class loginActivity : AppCompatActivity() {
         Login = findViewById(R.id.btnLogin)
         register = findViewById(R.id.btnreg)
 
+        db = openOrCreateDatabase("carmenDB", Context.MODE_PRIVATE, null)
+        
 
         Login.setOnClickListener {
+            var email = Email.text.toString().trim()
+            var passlgn = Password.text.toString().trim()
+
+            if (email.isEmpty() || passlgn.isEmpty()) {
+
+                Toast.makeText(this, "CANNOT SUBMIT EMPTY TEXT", Toast.LENGTH_SHORT).show()
+            } else {
+                val cursor = db.rawQuery(
+                    "SELECT * FROM users WHERE arafa=? AND nenosiri=?",
+                    arrayOf(email, passlgn)
+                )
+
+                if (cursor != null && cursor.moveToFirst()) {
+                    // user is authenticated, start a new activity
+                    val intent = Intent(this, Dashboard::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Invalid email or password, please try again",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
 
 
+
+
+            register.setOnClickListener {
+
+                var gotomain = Intent(this, MainActivity::class.java)
+                startActivity(gotomain)
+
+
+            }
 
         }
-        register.setOnClickListener {
-
-           var gotomain = Intent(this,MainActivity ::class.java)
-            startActivity(gotomain)
-
-
-        }
-
     }
 }
